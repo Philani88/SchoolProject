14676de103769e9cf2baa05034b365e1679ba873
Philani88 <61004912+Philani88@users.noreply.github.com>
2023-04-18 15:31:27 +0200

    Update Login-form.php

--- Login-form.php
+++ Login-form.php
@@ -1,39 +1,51 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-	  <meta charset = "utf-8">
-	  <title>LOGIN_SESSION</title>
-	</head>
-	<body>
-	  <form method = "POST" action = "processlogin.php" role = "form" class = "form-horizontal">
-		<table class = "form-header">
-		  <div class = "form-header">
-		    <h2>Space for other atributes:</h2><hr/>	
-		  </div>
-		<div class = "form-group">
-		  <tr id = "Row-input">
-		    <td>
-			  <div class = "col-md">
-			    <label for = "first" class= "label">Learner Number</label>
-			      <input type="text" class = "form-control" name = "first" >
-			  </div>
-			</td>
-		    <td>
-			  <div class = "col-md">
-			    <label for="first" class="label">Password</label>
-			    <input type="password" class="form-control" name = "password" >
-			  </div>
-			</td>
-		  </tr> 
-		  <tr>
-		    <td>
-		      <div class ="col-md">
-		        <button type = "submit" class = "button">Submit</button>
-			  </div>
-			</td>
-		  </tr>
-		</div>
-		</table>
-	  </form>
-	</body>
-  </html>
+<html>
+<head>
+<link rel="stylesheet" href="loginFormStyle.css">
+<title> CSS Login Screen Tutorial </title>
+</head>
+<body>
+  <body>
+    <div class="login-page">
+      <div class="form">
+        <div class="login">
+          <div class="login-header">
+            <h3>LOGIN</h3>
+            <p>Please enter your credentials to login.</p>
+          </div>
+        </div>
+        <form class="login-form">
+          <input type="text" placeholder="username"/>
+          <input type="password" placeholder="password"/>
+          <button>login</button>
+          <p class="message">Not registered? <a href="#">Create an account</a></p>
+        </form>
+      </div>
+    </div>
+</body>
+</body>
+<footer>
+ <footer>
+	  <div class = "fumuRow"  >
+	 
+	    <nav align = "center" style = "padding-top:10%">
+		  
+		  <img  src="img/footerLogo.png" width = "25%">
+		  
+		</nav>
+		<nav align = "left" style = "padding-right:10%;padding-bottom:10%">
+		   <p align = "left">Connnect with us:</p>
+		   <a href = "#"><img style = "padding-right:0.05%;padding-bottom:0.05%" src="img/facebookIcon.png" width = "2.5%"></a>
+		   <a href = "#"><img style = "padding-right:0.05%" src="img/tweeterIcon.png" width = "2.68%"></a>
+		   <a href = "#"><img style = "padding-right:0.05%" src="img/instagramIcon.png" width = "2.75%"></a>
+		   <a href = "#"><img style = "padding-right:0.05%;padding- bottom:0.05%" src="img/gmailIcon.png" width = "2.8%"></a>
+		   <a href = "#"><img style = "padding-top:0.25%" src="img/youtubeIcon.png" width = "2.75%"></a>
+		   <a href = "#"><img style = "padding-left:0.02%;padding-bottom:0.035%" src="img/linkedinIcon.png" width = "2.68%"></a>
+		   <a href = "#"> <img style = "padding-left:0.02%;padding-bottom:0.05%" src="img/whatsAppIcon.png" width = "2.3%"> </a>
+		</nav>
+	  <nav>
+	   <p style = "color:white" align = "center" > © 2023 fumu web appplications </p>
+	  </nav>
+	</footer>
+</footer>
+</html>
